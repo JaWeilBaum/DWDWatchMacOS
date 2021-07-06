7823e979c87fb014251d390d245d7df09cb22f69
Leon Friedmann <leon.friedmann@tum.de>
2021-07-06 18:16:09 +0200

    Initial Commit

---
+++ DWDWatch/DWDWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  DWDWatchApp.swift
+//  DWDWatch
+//
+//  Created by Léon Friedmann on 06.07.21.
+//
+
+import SwiftUI
+
+@main
+struct DWDWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
